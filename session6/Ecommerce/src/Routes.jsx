6af1529432f3aa5bb7d8daaae15a4157d6fb6c44
Dharaneesh-Selvaraj <dharaneeshselvaraj@gmail.com>
2025-05-08 11:45:43 +0530

    UseEffect component and building a signup page using use state and tasked to validate the input fields

--- session6/Ecommerce/src/Routes.jsx
+++ session6/Ecommerce/src/Routes.jsx
@@ -4,11 +4,15 @@ import Product from './Product'
 import Example from './Home'
 import Navbar from './Navbar'
 import AllComponets from './AllComponets'
+import Register from './Signup/Register'
 
 export default function WebRoutes() {
   return (
     <BrowserRouter>  
         <Routes>
+          <Route path='/' element={<Register />}>
+
+          </Route>
           <Route path='buyer' element={<AllComponets />}>
             {/* OUTLET */}
             <Route index element={<Example />}></Route>
